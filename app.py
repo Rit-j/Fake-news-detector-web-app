5beadcafd42ae17ff79f7c4774c80496b3929bfd
Rit-j <joshiritik557@gmail.com>
2021-11-12 11:59:42 +0530

    update code

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ model = pickle.load(open("finalized_model.pkl", 'rb'))
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
 
 @app.route("/prediction", methods=['GET','POST'])
@@ -18,10 +18,15 @@ def prediction():
         predict = model.predict(vector.transform([news]))[0]
         print(predict)
 
-        return render_template("prediction.html", prediction_text="News is -> {}".format(predict))
+        return render_template("prediction.html", prediction_text="This news is - {}".format(predict))
         
     else:
         return render_template("prediction.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")        
 
 if __name__ == '__main__':
     app.run()    
